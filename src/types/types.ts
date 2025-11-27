9e505ecf75591c5ae509a1564d91b22e8678bcb8
prinkley <prinkley@yandex.ru>
2025-11-27 16:48:28 +0300

    типы маринина

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,15 @@
-export type sum = (a: number, b: number) => number
+export type ValidationRule = {
+    rule: string;
+    value: any;
+    errorMessage: string;
+}
+
+export type FieldConfig = {
+    rules: ValidationRule[];
+    errorContainer: string | HTMLElement;
+}
+
+export type ValidationResult = {
+  isValid: boolean;                
+  errors: { field: string; message: string }[]; 
+}
