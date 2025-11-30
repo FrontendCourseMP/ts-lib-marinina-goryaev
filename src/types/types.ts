dcc26387962447304884c80ef0ecc7144f1a21b5
vadim <vadimtop228pc@gmail.com>
2025-11-30 22:29:42 +0300

    типы доделали Горяев

--- src/types/types.ts
+++ src/types/types.ts
@@ -13,3 +13,23 @@ export type ValidationResult = {
   isValid: boolean;                
   errors: { field: string; message: string }[]; 
 }
+
+export type ValidationConfig = {
+  validateOnBlur?: boolean;
+  validateOnChange?: boolean;
+  focusInvalidField?: boolean;
+  lockForm?: boolean;
+  errorFieldCssClass?: string;
+  errorLabelCssClass?: string;
+  successFieldCssClass?: string;
+  successLabelCssClass?: string;
+};
+
+export type FormFields = {
+  [fieldName: string]: FieldConfig;
+};
+
+export type Validator = {
+  addField: (field: string, config: FieldConfig) => void;
+  validate: () => ValidationResult;
+};
